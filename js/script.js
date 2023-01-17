1fe08187adcc20399fe833f10100924912ef9fb4
Hae Choi <haechoi92@yahoo.com>
2023-01-16 19:51:08 -0800

    add js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const titles = document.querySelectorAll(".title-row");
+
+titles.forEach((title) => {
+  title.addEventListener("click", () => {
+    if (title.classList.contains("is-open")) {
+      title.classList.remove("is-open");
+    } else {
+      const titlesWithIsOpen = document.querySelectorAll(".is-open");
+      titlesWithIsOpen.forEach((titleWithIsOpen) => {
+        titleWithIsOpen.classList.remove("is-open");
+      });
+      title.classList.add("is-open");
+    }
+  });
+});
