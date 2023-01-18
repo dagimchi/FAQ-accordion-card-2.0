9a07cb8c46b602ef84e2e13bddf3d46d2f4bd52b
Hae Choi <haechoi92@yahoo.com>
2023-01-17 17:31:51 -0800

    add comments

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@
 
 const titles = document.querySelectorAll(".title-row");
 
+// ITERATE THROUGH THE NODE AND APPLY A CLICK EVENT
 titles.forEach((title) => {
   title.addEventListener("click", () => {
+    // IF PANEL IS ALREADY OPEN WHEN CLICKED, CLOSE SAID PANEL
     if (title.classList.contains("is-open")) {
       title.classList.remove("is-open");
     } else {
+      // IF PANEL IS NOT OPEN WHEN CLICKED, OPEN IT AND CLOSE ANY OTHER OPEN PANELS
       const titlesWithIsOpen = document.querySelectorAll(".is-open");
       titlesWithIsOpen.forEach((titleWithIsOpen) => {
         titleWithIsOpen.classList.remove("is-open");
       });
       title.classList.add("is-open");
-      let panel = document.querySelector(".content");
     }
   });
 });
